4539bb15fefd01c08aa6626ba696a7523b884848
Denisa Arsene <e.d.arsene-1@student.tudelft.nl>
2023-03-03 16:38:57 +0100

    added basic classes

---
+++ commons/src/Column.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Column {
+    private String title;
+    private List<Card>cards;
+
+    public Column(String title, List<Card> cards) {
+        this.title = title;
+        this.cards = new ArrayList<>();
+    }
+
+}
