7962dd2ec817796f8a5af441d1859d822ea7fb69
Cristian Benghe <c.benghe@student.tudelft.nl>
2023-03-09 17:10:47 +0100

    Add controllers for the second and third scenes (MainOverview and BoardOverview

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -15,6 +15,7 @@
  */
 package client.scenes;
 
+import commons.Board;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -30,16 +31,30 @@ public class MainCtrl {
     private AddQuoteCtrl addCtrl;
     private Scene add;
 
+    private BoardOverviewCtrl boardOverviewCtrl;
+    private Scene boardOverview;
+
+    private MainOverviewCtrl mainOverviewCtrl;
+    private Scene mainOverview;
+
     public void initialize(Stage primaryStage, Pair<QuoteOverviewCtrl, Parent> overview,
-            Pair<AddQuoteCtrl, Parent> add) {
+            Pair<AddQuoteCtrl, Parent> add, Pair<MainOverviewCtrl, Parent> mainOverview,
+                           Pair<BoardOverviewCtrl, Parent> boardOverview) {
         this.primaryStage = primaryStage;
+
         this.overviewCtrl = overview.getKey();
         this.overview = new Scene(overview.getValue());
 
         this.addCtrl = add.getKey();
         this.add = new Scene(add.getValue());
 
-        showOverview();
+        this.boardOverviewCtrl = boardOverview.getKey();
+        this.boardOverview = new Scene(boardOverview.getValue());
+
+        this.mainOverviewCtrl = mainOverview.getKey();
+        this.mainOverview = new Scene(mainOverview.getValue());
+
+        showOverview(); //showClientConnect();
         primaryStage.show();
     }
 
@@ -53,5 +68,20 @@ public class MainCtrl {
         primaryStage.setTitle("Quotes: Adding Quote");
         primaryStage.setScene(add);
         add.setOnKeyPressed(e -> addCtrl.keyPressed(e));
+    }
+
+    public void showBoardOverview() {
+        primaryStage.setTitle("Your Board");
+        primaryStage.setScene(boardOverview);
+    }
+
+    public void showMainOverview() {
+        primaryStage.setTitle("Home");
+        primaryStage.setScene(mainOverview);
+    }
+
+    //TODO
+    public void showClientConnect() {
+
     }
 }
