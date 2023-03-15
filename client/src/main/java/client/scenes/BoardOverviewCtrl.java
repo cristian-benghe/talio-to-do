abd3fa08bd50f5aca3645476b9c07062bb33e8ef
Denisa Arsene <e.d.arsene-1@student.tudelft.nl>
2023-03-15 22:42:57 +0200

    solve checkstyle errors

--- client/src/main/java/client/scenes/BoardOverviewCtrl.java
+++ client/src/main/java/client/scenes/BoardOverviewCtrl.java
@@ -5,20 +5,15 @@ import com.google.inject.Inject;
 import commons.Board;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Orientation;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 
 public class BoardOverviewCtrl implements Initializable {
     private Long ind= Long.valueOf(0);
