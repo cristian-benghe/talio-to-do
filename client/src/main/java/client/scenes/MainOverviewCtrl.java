e832057bc27cea86d55b3b52458f1c4367352c59
Cristian Benghe <c.benghe@student.tudelft.nl>
2023-04-03 09:04:52 +0200

    Fix checkstyle
    Former-commit-id: a6544293ffd79b42372cdd5f995dc023c0c3be19

--- client/src/main/java/client/scenes/MainOverviewCtrl.java
+++ client/src/main/java/client/scenes/MainOverviewCtrl.java
@@ -12,7 +12,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
