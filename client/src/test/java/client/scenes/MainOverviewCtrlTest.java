fe4959a57606f62421e0ad5777d6928f4e95b6b8
Halid ÇIRAK <m.h.cirak@student.tudelft.nl>
2023-04-03 16:37:34 +0200

    Delete unnecessary methods and add some test templates
    Former-commit-id: 4eba56dd787c2a0a8054664e8d710d4d488ce6de

---
+++ client/src/test/java/client/scenes/MainOverviewCtrlTest.java
@@ -0,0 +1,68 @@
+package client.scenes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainOverviewCtrlTest {
+
+    @Test
+    void showOverview() {
+    }
+
+    @Test
+    void refreshOverview() {
+    }
+
+    @Test
+    void updateBoardsList() {
+    }
+
+    @Test
+    void disconnect() {
+    }
+
+    @Test
+    void updateSearchConstraintText() {
+    }
+
+    @Test
+    void updateBoardsText() {
+    }
+
+    @Test
+    void searchBoard() {
+    }
+
+    @Test
+    void createBoard() {
+    }
+
+    @Test
+    void onBoardClick() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void socketsCall() {
+    }
+
+    @Test
+    void loadUserWorkspace() {
+    }
+
+    @Test
+    void refreshWorkspaceFile() {
+    }
+
+    @Test
+    void setConnection() {
+    }
+
+    @Test
+    void getServer() {
+    }
+}
