da2a6c54a6c8c475b17b8ffe61ac852a0e99182c
Cristian Benghe <c.benghe@student.tudelft.nl>
2023-04-11 16:54:51 +0200

    Fix bug related to admin boards
    Former-commit-id: d6f613d72fc2ad6e59181d81d4cd0c47acf3da6d

--- client/src/main/java/client/scenes/MainOverviewCtrl.java
+++ client/src/main/java/client/scenes/MainOverviewCtrl.java
@@ -122,6 +122,12 @@ public class MainOverviewCtrl implements Initializable {
                 boardsListElement.setItems(null);
                 return;
             }
+        }else  {
+            if (availableBoards == null || availableBoards.isEmpty()) {
+                emptyBoardListMsg.setVisible(true);
+                boardsListElement.setItems(null);
+                return;
+            }
         }
         emptyBoardListMsg.setVisible(false);
         //Convert all the boards' title&id into an ObservableList
@@ -494,10 +500,7 @@ public class MainOverviewCtrl implements Initializable {
                 // System.out.println(availableBoards+" "+availableUserBoards);
                 availableBoards.remove(toBeDeleted);
                 availableUserBoards.remove(toBeDeleted);
-                refreshOverview();
             }
-            //System.out.println("Deleted board " + toBeDeleted.toStringShort());
-
             Platform.runLater(this::refreshOverview);
         });
         server.registerForMessages("/topic/boards", Board.class, board -> {
